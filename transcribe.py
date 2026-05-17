e757ebb88196a8632de4f79dc9f12e3f7d568cc6
Ritesh <reyansh.mudgil3003@gmail.com>
2026-05-17 11:59:26 +0530

    reduced whisper model

--- transcribe.py
+++ transcribe.py
@@ -14,7 +14,7 @@ print("Loading Whisper model...")
 
 # You can change:
 # tiny / base / small / medium
-model = whisper.load_model("medium")
+model = whisper.load_model("tiny")
 
 print("Model loaded successfully!")
 
